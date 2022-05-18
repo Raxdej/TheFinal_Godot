53e3d3cbf5b8240a758be2b4240baf8a188038cc
Raxdej <D3R4JX@gmail.com>
2022-05-17 23:15:23 -0500

    Proyect's first version

---
+++ World/Player.gd
@@ -0,0 +1,77 @@
+extends KinematicBody2D
+
+const speed = 200
+
+var direction = 0 
+var isAttaking = false #Wait until attack animation end
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	#Direction (0 = Down) ( 1 = up) (2 = left) (3 = right) 
+	
+
+#Keyboard Input
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+		direction = 2
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+		direction = 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+		direction = 3
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+		direction = 0
+	if Input.is_action_pressed("ui_accept") && (velocity == Vector2(0, 0)):
+		attack_animation(direction)
+		
+	
+	
+	move_and_slide(velocity.normalized()*speed)
+	player_animation(velocity)
+	
+
+#Animation Movement!!
+func player_animation(velocity):
+	if velocity.y > 0:
+		$"AnimatedSprite".play("MoveDown")
+	elif velocity.y < 0:
+		$"AnimatedSprite".play("MoveUp")
+	elif velocity.x != 0:
+		$"AnimatedSprite".play("MoveSide")
+		$"AnimatedSprite".flip_h = velocity.x > 0
+	elif isAttaking == false:
+		$"AnimatedSprite".stop()
+		$"AnimatedSprite".frame = 0
+
+#Make the attack animation depending on where it points
+func attack_animation(direction):
+	match direction:
+		0:
+			$"AnimatedSprite".play("AttackDown")
+			isAttaking = true
+			$Hitbox/AttackDown.disabled = false
+		1:
+			$"AnimatedSprite".play("AttackUp")
+			$Hitbox/AttackUp.disabled = false
+			isAttaking = true
+		2:
+			$"AnimatedSprite".play("AttackSide")
+			$Hitbox/AttackLeft.disabled = false
+			isAttaking = true
+		3: 
+			$"AnimatedSprite".play("AttackSide")
+			$"AnimatedSprite".flip_h
+			$Hitbox/AttackRight.disabled = false
+			isAttaking = true
+		
+	
+
+func _on_AnimatedSprite_animation_finished():
+	if ($"AnimatedSprite".animation == "AttackDown") or ($"AnimatedSprite".animation == "AttackUp") or ($"AnimatedSprite".animation == "AttackSide"):
+		$Hitbox/AttackDown.disabled = true
+		$Hitbox/AttackLeft.disabled = true
+		$Hitbox/AttackRight.disabled = true
+		$Hitbox/AttackUp.disabled = true
+		isAttaking = false 
